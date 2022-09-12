7b3345ca4074576df2c16b94b632c0c28b12cb2c
Vindiza Aziz <vindizaaziz@gmail.com>
2022-09-12 19:41:45 +0600

    delete css

--- src/Layout/Header/Header.js
+++ src/Layout/Header/Header.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import {NavLink} from "react-router-dom";
-
+import logo from "./logo.svg"
 
 const Header = () => {
     return (
 
         <header className='header'>
             <div className="container">
-                <NavLink className="header__links-item" to="/reg">About Us</NavLink>
-                <NavLink className="header__links-item" to="/contact-us">Contact Us</NavLink>
-                <NavLink className="header__links-item" to="/portfolio">Portfolio</NavLink>
-                <NavLink className="header__links-item" to="/videos">Videos</NavLink>
-                <NavLink className="header__links-item" to="/book-now">Book Now</NavLink>
+                <div className="header__logo">
+                    <img src={logo} alt="Future Academy"/>
+                </div>
+                <nav className="header__nav">
+                    <NavLink className="header__links-item" to="/reg">About Us</NavLink>
+                    <NavLink className="header__links-item" to="/contact-us">Contact Us</NavLink>
+                    <NavLink className="header__links-item" to="/portfolio">Portfolio</NavLink>
+                    <NavLink className="header__links-item" to="/videos">Videos</NavLink>
+                    <NavLink className="header__links-item" to="/book-now">Book Now</NavLink>
+                </nav>
             </div>
 
 
